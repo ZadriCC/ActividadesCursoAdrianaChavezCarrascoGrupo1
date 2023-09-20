48da8713f8faa2ec768572b85b8abff54af9ef83
Adriana Chávez Carrasco <142181586+ZadriCC@users.noreply.github.com>
2023-09-19 18:44:05 -0600

    Delete Assesment Diagnostico/task-manager directory

--- Assesment Diagnostico/task-manager/src/main/java/com/assessment/repositorio/TareaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.assessment.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.assessment.entidades.Tarea;
-
-public interface TareaRepositorio extends JpaRepository<Tarea, Long>{
-
-}
