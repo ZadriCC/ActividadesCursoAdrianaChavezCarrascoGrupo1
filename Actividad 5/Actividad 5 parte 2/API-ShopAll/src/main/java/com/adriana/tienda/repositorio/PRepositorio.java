2262a422e265ea487f45bce3beeabe90300f7372
Adriana <ch.adri@hotmail.com>
2023-11-23 20:25:39 -0600

    Este no

--- Actividad 5/Actividad 5 parte 2/API-ShopAll/src/main/java/com/adriana/tienda/repositorio/PRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.adriana.tienda.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adriana.tienda.datos.Productos;
-
-public interface PRepositorio extends JpaRepository<Productos, Integer> {
-
-}
--- Actividad 5/Actividad 5 parte 2/API-ShopAll/src/main/java/com/adriana/tienda/repositorio/PdRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.adriana.tienda.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adriana.tienda.datos.Pedidos;
-
-public interface PdRepositorio extends JpaRepository<Pedidos, Integer> {
-
-}
