5455d18239cd2eb625e8fa48e81bf8b7f6b1f1cd
Adriana <ch.adri@hotmail.com>
2023-11-23 21:54:25 -0600

    Cambios
    Aqui tenia varios proyectos que eran lo mismo por eso los borro

--- Actividad 5/Actividad 5 parte 2/API-ShopAll/src/main/java/com/adriana/tienda/repositorio/SRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.adriana.tienda.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adriana.tienda.datos.Subcategoria;
-
-public interface SRepositorio extends JpaRepository<Subcategoria, Integer> {
-
-}
